8fcf36580515bdee86380195a3dc03f4a4ea08c6
Manuel Alabor <msites@msites.net>
2012-10-08 15:04:42 +0200

    addtional container for demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,31 +11,46 @@ define('widgeterydemo', function(require,exports,module) {
 	// Prepare parent:
 	var widgetToDrag = undefined;
 	parent.setPosition(10,10);
-	parent.setSize(500,500);
+	parent.setSize(400,400);
 	parent.setDraggable(true);
+	var initialClick;
 	parent.on('mouse:dragrequest', function(event) {
 		widgetToDrag = canvasWrapper.searchDeepestWidgetOnPosition(event.absolutePosition);
 		
 		if(widgetToDrag !== parent) {
 			event.acceptCallback(parent,widgetToDrag);
 			event.stopBubbling();
+			initialClick = event.absolutePosition;
 		}
 	});
 	parent.on('mouse:drag', function(event) {
-		widgetToDrag.setPosition(event.absolutePosition.left, event.absolutePosition.top);
-		canvasWrapper.redraw();
-		event.stopBubbling();
+		var parentLeft = 10;
+		var parentTop = 10;
+		var left = event.absolutePosition.left - parentLeft;
+		var top = event.absolutePosition.top - parentTop;
+		
+		widgetToDrag.setPosition(left, top);
 	});
 	
 
 	// Add some Labels:
 	for(var i = 0, l = 5; i<l; i++) {
-		var label = new Label('Drag Me! I\'m #' + i);
-		label.setPosition(20,i*50+20);
+		var y = i*50+20;
+		var label = new Label('20/' + y);
+		label.setPosition(20,y);
 		label.setSize(150,20);
+		
+		label.on('widget:moved', function(e) {
+			e.widget.setText(e.newPosition.left + '/' + e.newPosition.top);
+		});
+		
 		parent.addWidget(label);
 	}
 	
-	canvasWrapper.addWidget(parent);
+	var additionalParent = new Rectangle();
+	additionalParent.setSize(500,500)
+	additionalParent.setPosition(10,10);
+	additionalParent.addWidget(parent);
+	canvasWrapper.addWidget(additionalParent);
 	canvasWrapper.redraw();
 });
