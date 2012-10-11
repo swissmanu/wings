73a9d07e6dc35392a3101be27da4e840a4a5dad0
Manuel Alabor <msites@msites.net>
2012-10-11 13:28:36 +0200

    added horizontal margin & unit test cases to boxlayout

--- test/spec/wings/layout/BoxLayout.js
+++ test/spec/wings/layout/BoxLayout.js
@@ -12,27 +12,73 @@ define(['wings/layout/LayoutManager','wings/layout/BoxLayout','wings/Widget','wi
 		});
 		
 		describe('layoutWidgets(parent,widgets)', function() {
+			var parent;
+			var widget1;
+			var widget2;
+			var widget3;
+			
+			beforeEach(function() {
+				parent = new Panel();
+				widget1 = new Widget();
+				widget2 = new Widget();
+				widget3 = new Widget();
+				
+				widget1.setSize(40,20);
+				widget2.setSize(10,20);
+				widget3.setSize(30,20);
+			});
+			
 			it('should not throw an error signaling missing implementation', function() {
 				(function() {
 					new BoxLayout().layoutWidgets(new Widget(), []);
 				}).should.not.throw();
 			});
 			
-			
-			it('should lay out widgets correct', function() {
-				var parent = new Panel();
-				var widget1 = new Widget();
-				var widget2 = new Widget();
-				var boxLayout = new BoxLayout();
-				
+			it('should lay out widgets without horizontal margin correctly', function() {
+				var boxLayout = new BoxLayout(0);
 				parent.setSize(200,50);
-				widget1.setSize(40,20);
-				widget2.setSize(40,20);
 				
-				boxLayout.layoutWidgets(parent, [widget1,widget2]);
+				var expectedLeftPositionWidget1 = 0;
+				var expectedLeftPositionWidget2 = widget1.getSize().width;
+				var expectedLeftPositionWidget3 = widget1.getSize().width + widget2.getSize().width;
 				
-				widget1.getPosition().should.eql({left:0,top:0});
-				widget2.getPosition().should.eql({left:40,top:0});
+				boxLayout.layoutWidgets(parent, [widget1,widget2,widget3]);
+				
+				widget1.getPosition().should.eql({left:expectedLeftPositionWidget1,top:0});
+				widget2.getPosition().should.eql({left:expectedLeftPositionWidget2,top:0});
+				widget3.getPosition().should.eql({left:expectedLeftPositionWidget3,top:0});
+			});
+			
+			it('should lay out widgets with default horizontal margin correctly', function() {
+				var boxLayout = new BoxLayout();
+				var defaultMargin = 5;
+				parent.setSize(200,50);
+				
+				var expectedLeftPositionWidget1 = 0;
+				var expectedLeftPositionWidget2 = defaultMargin + widget1.getSize().width;
+				var expectedLeftPositionWidget3 = 2*defaultMargin + widget1.getSize().width + widget2.getSize().width;
+				
+				boxLayout.layoutWidgets(parent, [widget1,widget2,widget3]);
+				
+				widget1.getPosition().should.eql({left:expectedLeftPositionWidget1,top:0});
+				widget2.getPosition().should.eql({left:expectedLeftPositionWidget2,top:0});
+				widget3.getPosition().should.eql({left:expectedLeftPositionWidget3,top:0});
+			});
+			
+			it('should lay out widgets with specified horizontal margin correctly', function() {
+				var margin = 10;
+				var boxLayout = new BoxLayout(margin);
+				parent.setSize(200,50);
+				
+				var expectedLeftPositionWidget1 = 0;
+				var expectedLeftPositionWidget2 = margin + widget1.getSize().width;
+				var expectedLeftPositionWidget3 = 2*margin + widget1.getSize().width + widget2.getSize().width;
+				
+				boxLayout.layoutWidgets(parent, [widget1,widget2,widget3]);
+				
+				widget1.getPosition().should.eql({left:expectedLeftPositionWidget1,top:0});
+				widget2.getPosition().should.eql({left:expectedLeftPositionWidget2,top:0});
+				widget3.getPosition().should.eql({left:expectedLeftPositionWidget3,top:0});
 			});
 			
 		});
