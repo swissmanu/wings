6b4d4367ac9da4993039df43a8cb92144cb17d54
Manuel Alabor <msites@msites.net>
2012-10-11 13:03:20 +0200

    implemented horizontal placement for boxlayout

--- test/spec/wings/layout/BoxLayout.js
+++ test/spec/wings/layout/BoxLayout.js
@@ -1,4 +1,4 @@
-define(['wings/layout/LayoutManager','wings/layout/BoxLayout','wings/Widget'], function (LayoutManager, BoxLayout,Widget) {
+define(['wings/layout/LayoutManager','wings/layout/BoxLayout','wings/Widget','wings/Panel'], function (LayoutManager, BoxLayout,Widget,Panel) {
 
     describe('BoxLayout', function() {
         it('should be an LayoutManager', function() {
@@ -14,9 +14,27 @@ define(['wings/layout/LayoutManager','wings/layout/BoxLayout','wings/Widget'], f
 		describe('layoutWidgets(parent,widgets)', function() {
 			it('should not throw an error signaling missing implementation', function() {
 				(function() {
-					new BoxLayout().layoutWidgets();
+					new BoxLayout().layoutWidgets(new Widget(), []);
 				}).should.not.throw();
 			});
+			
+			
+			it('should lay out widgets correct', function() {
+				var parent = new Panel();
+				var widget1 = new Widget();
+				var widget2 = new Widget();
+				var boxLayout = new BoxLayout();
+				
+				parent.setSize(200,50);
+				widget1.setSize(40,20);
+				widget2.setSize(40,20);
+				
+				boxLayout.layoutWidgets(parent, [widget1,widget2]);
+				
+				widget1.getPosition().should.eql({left:0,top:0});
+				widget2.getPosition().should.eql({left:40,top:0});
+			});
+			
 		});
 		
     });
